8fdf57db3584a2ca5b9ebf784237eead5b5b292a
priyanka2096 <58855479+priyanka2096@users.noreply.github.com>
2022-02-14 18:34:51 +0530

    m

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,36 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { OrderComponent } from './order/order.component';
+import { FeedbackComponent } from './feedback/feedback.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  
+  {
+    path:'',
+    component: HomeComponent
+  },
+
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+
+  {
+    path: 'order',
+    component: OrderComponent
+  },
+
+  {
+    path: 'feedback',
+    component: FeedbackComponent
+  },
+
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { OrderComponent } from './order/order.component';
+import { AboutComponent } from './about/about.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    OrderComponent,
+    AboutComponent,
+    FeedbackComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
